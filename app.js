faea6ba002f92a787f6ae3d0551029286b1c972f
mandros1 <marinandros031@gmail.com>
2019-12-01 00:29:22 +0100

    minor HTML rework and CSS implementation, moving toward dynamic content

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const apiPort = process.env.API_PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
@@ -23,4 +29,3 @@ app.use((req, res, next) => {
 app.listen(apiPort, () => {
     console.log(`Server is up and running on localhost:${apiPort}`)
 });
-
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
+const rootDirPath = require('../utils/path');
 
 const express = require('express');
 
 const router = express.Router();
 
 router.get( '/', (req, res, next) =>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDirPath, 'views', 'shop.html'));
 });
 
 module.exports = router;
